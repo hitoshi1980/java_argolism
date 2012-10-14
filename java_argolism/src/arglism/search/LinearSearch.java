9993e6a714bc75a41a3cb99ec3121e0c858f2f51
hitoshi <hito_o1980@yahoo.co.jp>
2012-10-14 14:45:00 +0900

    commit liner search

---
+++ java_argolism/src/arglism/search/LinearSearch.java
@@ -0,0 +1,31 @@
+package arglism.search;
+
+public class LinearSearch {
+	
+	/** 発見できない場合 **/
+	private static final int NOT_FOUND = -1;
+
+	public static int find (int x, int[] a, int left, int right) {
+		
+		while (left < right) {
+			
+			int mid = (left + right) / 2;
+			
+			if (a[mid] < x) {
+				left = mid + 1;
+			}
+			else {
+				right = mid;
+			}			
+		}
+		
+		if (a[left] == x) {
+			return left;
+		}
+		return NOT_FOUND;
+	}
+	
+	private static void main(String args) {
+		
+	}
+}
